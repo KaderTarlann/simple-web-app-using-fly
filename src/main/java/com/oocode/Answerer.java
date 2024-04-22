0dfa6bd4c58d512984e4e94c194e6201fe60a8e3
KaderTarlann <kadertarlan1@gmail.com>
2024-04-22 16:49:59 +0100

    add additional questions and answers

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -2,6 +2,13 @@ package com.oocode;
 
 public class Answerer {
     public String answerFor(String question) {
-        return "Kader";
+        return switch (question) {
+            case "What is your name?" -> "Kader";
+            case "When were you born?" -> "1994";
+            case "Where did you born?" -> "Turkey";
+            case "What is your nationality?" -> "Turkey";
+            case "What is your citizenship?" -> "British";
+            default -> ("I cannot answer the question: " + question);
+        };
     }
 }
--- src/test/java/com/oocode/AnswererTest.java
+++ src/test/java/com/oocode/AnswererTest.java
@@ -8,8 +8,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class AnswererTest {
     @Test
     public void canAnswerMyName() {
-        var contents = new Answerer().answerFor("What is your name?");
+        var name = new Answerer().answerFor("What is your name?");
+        var birth = new Answerer().answerFor("When were you born?");
+        var born = new Answerer().answerFor("Where did you born?");
+        var nationality = new Answerer().answerFor("What is your nationality?");
+        var citizeniship = new Answerer().answerFor("What is your citizenship?");
 
-        assertThat(contents, equalTo("Kader"));
+        assertThat(name, equalTo("Kader"));
+        assertThat(birth, equalTo("1994"));
+        assertThat(born, equalTo("Turkey"));
+        assertThat(nationality, equalTo("Turkey"));
+        assertThat(citizeniship, equalTo("British"));
     }
 }
